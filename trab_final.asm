376eb4638cb908a7e77fa0aa65c130f3039e2547
Arthur Henrique Paulini Grasnievicz <98137147+arthurGNZ@users.noreply.github.com>
2024-09-17 16:27:22 -0300

    Update trab_final.asm

--- trab_final.asm
+++ trab_final.asm
@@ -1,3 +1,6 @@
+#Alunos: 
+#Arthur Henrique Paulini Grasnievicz - 2311100002
+#Gabriel Gois - 2311100030
 .data 
 tabuleiro:        .space    54     #0 de ninguém, X - Jogador 1, # - Jogador 2
 num_jogador:      .word     1
@@ -18,14 +21,9 @@ txt_config_tam:   .asciz "\n  - Tamanho do tabuleiro: "
 txt_config_con:   .asciz "\n  - Placar (Jogador 1 vs Jogador 2): "
 txt_opcao_inv:    .asciz "\nEscolha inválida! Voltando para o menu inicial\n"
 espaco:           .asciz " "
-placeholder_txt:             .asciz "\n Menu em construção voltando para menu inicial \n"
+quebra:	.asciz	"\n"
 
 .text
-_start:
-    j main_menu
-
-
-
 main_menu:
     la a0, txt_menu
     li a7, 4
@@ -42,7 +40,7 @@ main_menu:
     beq a0, t2, finish
 
     j error
-
+#--------------------	
 
 config_submenu:
     la a0, txt_submenu
@@ -65,7 +63,7 @@ config_submenu:
 
     j error
 
-
+#--------------------	
 config_jogadores:
     la a0, txt_jogadores
     li a7, 4
@@ -86,7 +84,7 @@ salva_jogador:
     sw a0, 0(t1)
     j main_menu
 
-
+#--------------------	
 
 config_tabuleiro:
     la a0, txt_tabuleiro
@@ -108,7 +106,7 @@ salva_tab:
     sw a0, 0(t1)
     j main_menu
 
-
+#--------------------	
 config_dificuldade:
     la a0, txt_dificuldade
     li a7, 4
@@ -129,7 +127,7 @@ salva_dif:
     sw a0, 0(t1)
     j main_menu
 
-
+#--------------------	
 
 zerar_contadores:
     li t0, 0
@@ -139,7 +137,7 @@ zerar_contadores:
     sw t0, 0(t1)
     j main_menu
 
-
+#--------------------	
 mostrar_config:
     la a0, txt_config
     li a7, 4
@@ -191,20 +189,78 @@ mostrar_config:
 
     j main_menu
 
-
+#--------------------	
 game_menu:
-    la a0, placeholder_txt
-    li a7, 4
-    ecall
-    j main_menu
+	la a0, tabuleiro #pos. de memória da matriz do tabuleiro
+	la a1, tamanho_tab 
+	lb a1, 0(a0) #quantidade de colunas do tabuleiro
+	call inicializa_tabuleiro
+	call imprime_tabuleiro
+	j main_menu
+#--------------------
+inicializa_tabuleiro:
+	li t0, 53 #tamanho do tabuleiro
+	li t1, 0 #var de controle do loop
+	j loop_inicializa
+loop_inicializa:
+	beq t0,t1,final_inicializa
+	add t2, t1, a0 #salvando a posição de memória da variável atual do loop em t2
+	li t3,0 #salvando 0 em t3 para salvar na posição de t2 futuramente
+	sb t3, 0(t2)
+	addi t1,t1,1 #andando 1 posição no loop
+	j loop_inicializa
+final_inicializa:
+	ret
+#--------------------	
+imprime_tabuleiro:
+	li t0, 6 #tamanho do primeiro loop(número de linhas)
+	li t1, 0 #variavel de controle do primeiro loop
+	
+	la t2, tamanho_tab
+	lb t2, 0(t2)#tamanho do segundo loop(número de colunas: 9 ou 7)
 
+	mv t6, a0 #tirando a posição inicial do tabuleiro de a0, pois ele será usado para fazer ecall
 
+	j loop1_imprimir
+	
+loop1_imprimir:
+	beq t0,t1, final_imprimir
+	li t3, 0 #variável de controle do segundo loop
+	j loop2_imprimir
+	
+loop2_imprimir:	
+	beq t2,t3, final_l2
+	mul t4, t1, t2 #t4 recebe linha_atual*tam_linha
+	add t4, t4, t3 #t4 recebe as linhas andadas + coluna atual
+	add t4, t4, t6 #t4 recebe pos_inicial do tabuleiro+quantidades de casa necessária para chegar na pos atual 
+
+	#ecall para imprimir valor atual 
+	lb a0, 0(t4)
+	li a7, 1
+	ecall
+	
+	#ecall para imprimir espaço
+	la a0, espaco
+	li a7, 4
+	ecall
+	
+	addi t3, t3, 1 #fazendo loop andar 1 posição
+	j loop2_imprimir
+final_l2:
+	addi t1,t1,1
+	la a0, quebra
+	li a7, 4
+	ecall
+	j loop1_imprimir
+final_imprimir:
+	ret
+#--------------------	
 error:
     la a0, txt_opcao_inv
     li a7, 4
     ecall
     j main_menu
-
+#--------------------	
 
 finish:
     li a7, 10
